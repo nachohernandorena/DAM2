af7031713cc48c31978f4349913dce124db8b829
nacho hernandorena <nash.gnr@gmail.com>
2023-08-09 21:10:44 -0300

    mysql-connector.js: change ip host

--- app-fullstack-base-2022-i07/src/backend/mysql-connector.js
+++ app-fullstack-base-2022-i07/src/backend/mysql-connector.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const configMysql = {
     connectionLimit: 10,
-    host: '192.168.0.45',
+    host: 'mysql-server',
     port: '3306',
     user: 'root',
     password: 'userpass',
